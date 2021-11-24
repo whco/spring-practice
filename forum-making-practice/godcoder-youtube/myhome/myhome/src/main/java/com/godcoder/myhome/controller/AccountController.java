8a109066915a471b3da62ace4b0c739a73a99ab0
whco <einsteized@gmail.com>
2021-11-24 23:35:21 +0900

    Feat: Add Login Layout

---
+++ forum-making-practice/godcoder-youtube/myhome/myhome/src/main/java/com/godcoder/myhome/controller/AccountController.java
@@ -0,0 +1,17 @@
+package com.godcoder.myhome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "account/login";
+    }
+}
+
